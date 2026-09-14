957d3d3bb1c573d95c315c8380d76c49f4335ecd
orionblur <252493356+orionblur@users.noreply.github.com>
2026-09-13 17:12:04 -0700

    feat: Update version and warning

--- src/Settings/ModernSettingsViewController.m
+++ src/Settings/ModernSettingsViewController.m
@@ -469,7 +469,7 @@
 }
 - (void)footerLabelLongPressed:(UILongPressGestureRecognizer *)sender {
     if (sender.state == UIGestureRecognizerStateBegan) {
-        NSURL *url = [NSURL URLWithString:@"https://www.youtube.com/watch?v=uFUSim9dvQw"];
+        NSURL *url = [NSURL URLWithString:@"https://www.youtube.com/watch?v=BWWfjK0v8eM"];
         if ([[UIApplication sharedApplication] canOpenURL:url]) {
             [[UIApplication sharedApplication] openURL:url options:@{} completionHandler:nil];
         }
